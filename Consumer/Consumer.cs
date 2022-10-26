44e3c8ec82216ca5e9265d2e54ab1a6b51eaaa92
Oladeji Mujib <oladejimujib@gmail.com>
2022-10-26 12:16:40 +0100

    Add project files.

---
+++ Consumer/Consumer.cs
@@ -0,0 +1,50 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+
+
+var clientConfig = new ClientConfig();
+clientConfig.BootstrapServers = "pkc-6ojv2.us-west4.gcp.confluent.cloud:9092";
+clientConfig.SecurityProtocol = SecurityProtocol.SaslSsl;
+clientConfig.SaslMechanism = SaslMechanism.Plain;
+clientConfig.SaslUsername = "SZTQHS22QZSEHBGW";
+clientConfig.SaslPassword = "5fJ9UHRER225K4IA3S/Tf2s8AxcSLtxUCEtKEuGTDrUgbh+5RFt4TMOIk8CI7jqb";
+clientConfig.SslCaLocation = "probe";
+
+var consumerConfig = new ConsumerConfig (clientConfig);
+
+consumerConfig.GroupId = "weather-consumer-group";
+consumerConfig.AutoOffsetReset = AutoOffsetReset.Earliest;
+
+using var consumer = new ConsumerBuilder<string,string>(consumerConfig).Build();
+
+consumer.Subscribe("weatherReport");
+
+CancellationTokenSource token = new();
+
+try
+{
+	while (true)
+	{
+		var response = consumer.Consume(token.Token);
+		if(response.Message is not null)
+		{
+			var weather = JsonConvert.DeserializeObject<Weather>(response.Message.Value);
+			Console.WriteLine($"State: {weather.State} \t Temperature: {weather.Temperature}F");
+		}
+	}
+}
+catch (Exception)
+{
+
+	throw;
+}
+
+finally 
+{
+	Console.WriteLine(nameof(consumer));
+	consumer.Close();
+}
+
+
+
+public record Weather(string State, int Temperature);
---
+++ KafkaImplementation/Producer.cs
@@ -0,0 +1,33 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+
+var clientConfig = new ClientConfig();
+clientConfig.BootstrapServers = "pkc-6ojv2.us-west4.gcp.confluent.cloud:9092";
+clientConfig.SecurityProtocol = SecurityProtocol.SaslSsl;
+clientConfig.SaslMechanism = SaslMechanism.Plain;
+clientConfig.SaslUsername = "SZTQHS22QZSEHBGW";
+clientConfig.SaslPassword = "5fJ9UHRER225K4IA3S/Tf2s8AxcSLtxUCEtKEuGTDrUgbh+5RFt4TMOIk8CI7jqb";
+clientConfig.SslCaLocation = "probe";
+
+using var producer = new ProducerBuilder<string, string>(clientConfig).Build();
+ 
+try
+{
+	string? state;
+	while ((state= Console.ReadLine()) != null)
+	{
+        var response = await producer.ProduceAsync("weatherReport", new Message<string, string> { Key = Guid.NewGuid().ToString().Replace("/", "-").Substring(5),
+									Value = JsonConvert.SerializeObject(new Weather(state, 70)) });
+		Console.WriteLine(response.Value);
+    }
+}
+catch (ProduceException<string, string> ex)
+{
+	Console.WriteLine(ex.Message);
+}
+finally
+{
+	Console.WriteLine("Done!");
+}
+
+public record Weather(string State, int Temperature);
